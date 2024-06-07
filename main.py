b7f4c63777c9bcf9b0e8ba069c9692362fa897cb
Clemens Helling <clemens.helling@icloud.com>
2024-06-07 22:40:42 +0200

    added geometry and ttk

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import tkinter as tk
-
+from tkinter import ttk
 root = tk.Tk()
-
-lable1 =  tk.Label(root, text="Hello World!")
-lable1.pack()
-
-
+style = ttk.Style()
+root.title("Hello World!")
+root.geometry("800x400")
+root.minsize(400, 200)
+root.maxsize(1200, 800)	
+label = ttk.Label(root, text="Hello World!", background="yellow")
+label.pack()
+#for item in label.keys():
+  #print(item, ":", label[item])
 
 root.mainloop()
