b9e71f79103fe4bbd44fb3fedbed9a3a8a5e902e
Clemens Helling <clemens.helling@icloud.com>
2024-06-08 15:28:35 +0200

    mehr Krankeihtsbilder hinzugefügt und eineige neue widgeds.

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 import tkinter as tk
 from tkinter import ttk
 
+def toggle_textfield(*args):
+    """Funktion, die ausgeführt wird, wenn sich der Zustand der Checkbox ändert."""
+    if checkbox_state.get():
+        textfield.grid(row=4, column=0, padx=10, pady=10)  # Zeigt das Textfeld an
+    else:
+        textfield.grid_remove()  # Versteckt das Textfeld
+
 root = tk.Tk()
-root.title("DiLer Medic")
+root.title("Notifer Base")
 root.geometry("800x600")
 
 # Erstellen Sie ein Frame-Widget
 frame = tk.Frame(root)
 frame.place(relx=0.5, rely=0.5, anchor='center')
 
-Titele = ttk.Label(frame, text="DiLer Medic", font=("Arial", 20))
+Titele = ttk.Label(frame, text="Notifer Base", font=("Arial", 20))
 Titele.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-Erkrankung_label = tk.Label(frame, text="Symptome")
-Erkrankung_label.grid(row=1, column=0, padx=10, pady=10)
 
-Erkrankung = ttk.Combobox(frame, text= "Symptome", values=["Herzinfarkt", "Schlaganfall", "Kreislaufstillstand"])
+
+Erkrankung = ttk.Combobox(frame, text= "Symptome", values=["Bauchschmerzen", "Kopfschmerzen", "Akutes Abdomen",
+"Intox","tee","Wärmflasche","ACS","Atemnot","Fraktur", "Sportverletzung","Synkope","Panikatake" , "Anaphilaktischer Schock"])
 Erkrankung.set("Wählen Sie eine Krankheit")  # Setzt den Standardwert
 Erkrankung.grid(row=1, column=0, padx=10, pady=10)
 
-name_label = ttk.Label(frame, text="Name")
-name_label.grid(row=2, column=0, padx=10, pady=10)
+unklare_lage_chackbox =ttk.Checkbutton(frame, text="Unklare Lage")
+unklare_lage_chackbox.grid(row=2, column=0, padx=10, pady=10)
 
-name = ttk.Entry(frame, text= "Name")
-name.insert(0,"Name")  # Fügt den Text "Name" am Anfang des Entry-Widgets ein
-name.grid(row=2, column=0, padx=10, pady=10)
 
-Alarm = tk.Button(frame, text="Alarmieren", command=lambda: print(Erkrankung.get()))
-Alarm.grid(row=3, column=0, padx=10, pady=10)
+
+# Erstellen Sie eine IntVar, um den Zustand der Checkbox zu verfolgen
+checkbox_state = tk.IntVar()
+
+# Erstellen Sie die Checkbox
+checkbox = ttk.Checkbutton(frame, text="Sonstige", variable=checkbox_state)
+checkbox.grid(row=3, column=0, padx=10, pady=10)
+
+# Erstellen Sie das Textfeld, aber zeigen Sie es zunächst nicht an
+textfield = ttk.Entry(frame, text = "Einsatzstichwort")
+# textfield.grid(row=5, column=0, padx=10, pady=10)  # Kommentiert aus, um das Textfeld zunächst zu verstecken
+
+# Rufen Sie die Funktion toggle_textfield auf, wenn sich der Zustand der Checkbox ändert
+checkbox_state.trace('w', toggle_textfield)
+
+
+
+Alarm = tk.Button(frame, text="Alarmieren", command=lambda: print(Erkrankung.get()), font=("Arial", 20), bg="red", fg="white")
+Alarm.grid(row=5, column=0, padx=10, pady=10)
 
 root.mainloop()
