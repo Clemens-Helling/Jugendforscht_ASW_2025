495a92fe8414c370d97c7ac2837c768695c50ece
Clemens-Helling <167019903+Clemens-Helling@users.noreply.github.com>
2024-07-28 12:15:56 +0200

    Error Nachricht geändert

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def show_page1():
     def show_error(message):
         global error_label
         if error_label is None:
-            error_label = tk.Label(frame, text=message, font=("Arial", 20), fg="red")
-            error_label.grid(row=6, column=0, padx=10, pady=10)
+            error_label = tk.Label(frame, text="Error:" + message, font=("Arial", 20), fg="red")
+            error_label.grid(row=0, column=0, padx=10, pady=10)
 
     button_text.set("Menü")
     def toggle_textfield(*args):
