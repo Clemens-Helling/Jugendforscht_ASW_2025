a3cad9cd67d06583ea44b2af1bd7ec5bc0ac806d
Clemens-Helling <167019903+Clemens-Helling@users.noreply.github.com>
2024-09-03 22:48:46 +0200

    main.py für bessere leslichkeit  in oop umgewandelt

--- allert/Alarm.py
+++
@@ -1,8 +0,0 @@
-from data.database import insert_alarm
-error =""
-def alarm(erkranung):
-    if erkranung == "Wählen Sie eine Krankheit":
-        error = "keine Krankheit ausgewählt"
-    else: insert_alarm(erkranung)
-    
-       
--- main.py
+++ main.py
@@ -1,122 +1,88 @@
 import tkinter as tk
 from tkinter import ttk
-from allert.Alarm import alarm , error
+from Alarm import alarm, error
+class AlarmApp:
+    def __init__(self, root):
+        self.root = root
+        self.frame = tk.Frame(root)
+        self.frame.pack()
 
-page =1
-is_error = False
-error_label = None
-# Tk Setup
-root = tk.Tk()
-root.title("Notifer Base")
-root.geometry("800x600")
-frame = tk.Frame(root)
-frame.place(relx=0.5, rely=0.5, anchor='center')
-button_text = tk.StringVar()
-global erkankung
-
-
-def get_erkrankung():
-        return erkrankung
-
-def togle_page():
-    global page
-    global button_text
-    global erkrankung
-    
-    if page == 1:
-        show_page2()
-        page = 2
+        self.unklare_lage_chackbox_state = tk.IntVar()
+        self.sonstiges_state = tk.IntVar()
+        self.frame.place(relx=0.5, rely=0.5, anchor='center')
+        self.create_widgets()
         
+    def create_widgets(self):
+        self.Titele = ttk.Label(self.frame, text="Notifer Base", font=("Arial", 20))
+        self.Titele.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
-    else:
-        show_page1()
-        page = 1
-    
-
-# Versteckt das Textfeld
-def show_page1():
-    global error_label
-    error_label = tk.Label(frame, font=("Arial", 20), fg="red")
-    def show_error(message):
-        error_label.config(text="Error:" + message)
-        error_label.grid(row=0, column=0, padx=10, pady=10)
-
-    button_text.set("Menü")
-    def toggle_textfield(*args):
-        if sonstiges_state.get():
-            textfield.grid(row=3, column=0, padx=10, pady=10)  # Zeigt das Textfeld an
-        else:
-            textfield.grid_remove()
-    
-
-    
-        
-
-    Titele = ttk.Label(frame, text="Notifer Base", font=("Arial", 20))
-    Titele.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-
-    Erkrankung = ttk.Combobox(frame, text= "Symptome", values=["Bauchschmerzen", "Kopfschmerzen", "Akutes Abdomen",
-    "Intox","tee","Wärmflasche","ACS","Atemnot","Fraktur", "Sportverletzung","Synkope","Panikatake" , "Anaphilaktischer Schock"])
+        self.Erkrankung = ttk.Combobox(self.frame, text= "Symptome", values=["Bauchschmerzen", "Kopfschmerzen", "Akutes Abdomen",
+        "Intox","tee","Wärmflasche","ACS","Atemnot","Fraktur", "Sportverletzung","Synkope","Panikatake" , "Anaphilaktischer Schock"])
 
  
-    Erkrankung.set("Wählen Sie eine Krankheit")  # Setzt den Standardwert
-    Erkrankung.grid(row=2, column=0, padx=10, pady=10)
+        self.Erkrankung.set("Wählen Sie eine Krankheit")  # Setzt den Standardwert
+        self.Erkrankung.grid(row=2, column=0, padx=10, pady=10)
+        # Erstellen der Checkbox für "Unklare Lage"
+        self.unklare_lage_chackbox = ttk.Checkbutton(self.frame, text="Unklare Lage", variable=self.unklare_lage_chackbox_state)
+        self.unklare_lage_chackbox.grid(row=3, column=0, padx=10, pady=10)
 
-    unklare_lage_chackbox_state = tk.IntVar()
+        # Erstellen der Checkbox für "Sonstige"
+        self.checkbox = ttk.Checkbutton(self.frame, text="Sonstige", variable=self.sonstiges_state)
+        self.checkbox.grid(row=4, column=0, padx=10, pady=10)
 
-    unklare_lage_chackbox =ttk.Checkbutton(frame, text="Unklare Lage", variable=unklare_lage_chackbox_state)
-    unklare_lage_chackbox.grid(row=3, column=0, padx=10, pady=10)
+        # Erstellen des Textfelds, aber zunächst nicht anzeigen
+        self.textfield = ttk.Entry(self.frame)
+        # self.textfield.grid(row=3, column=0, padx=10, pady=10)  # Kommentiert aus, um das Textfeld zunächst zu verstecken
 
-    
+        # Rufen Sie die Funktion toggle_textfield auf, wenn sich der Zustand der Checkbox ändert
+        self.sonstiges_state.trace('w', self.toggle_textfield)
 
+        # Erstellen des Labels für Fehlermeldungen
+        self.error_label = tk.Label(self.frame, text="Error:", font=("Arial", 20), fg="red")
+        self.error_label.grid(row=0, column=0, padx=10, pady=10)
+        self.error_label.grid_remove()
 
-    # Erstellen Sie eine IntVar, um den Zustand der Checkbox zu verfolgen
-    sonstiges_state = tk.IntVar()
+        # Erstellen des Buttons zum Auslösen des Alarms
+        self.button = tk.Button(self.frame, text="Alarmieren", command = self.get_alarm, font=("Arial", 20), bg="red", fg="white")
+        self.button.grid(row=6, column=0, padx=10, pady=10)
 
-    # Erstellen Sie die Checkbox
-    checkbox = ttk.Checkbutton(frame, text="Sonstige", variable=sonstiges_state)
-    checkbox.grid(row=4, column=0, padx=10, pady=10)
+    def toggle_textfield(self, *args):
+        if self.sonstiges_state.get():
+            self.textfield.grid(row=5, column=0, padx=10, pady=10)  # Zeigt das Textfeld an
+        else:
+            self.textfield.grid_remove()  # Versteckt das Textfeld
 
-    # Erstellen Sie das Textfeld, aber zeigen Sie es zunächst nicht an
-    textfield = ttk.Entry(frame, text = "Einsatzstichwort")
-    # textfield.grid(row=5, column=0, padx=10, pady=10)  # Kommentiert aus, um das Textfeld zunächst zu verstecken
-
-    # Rufen Sie die Funktion toggle_textfield auf, wenn sich der Zustand der Checkbox ändert
-    sonstiges_state.trace('w', toggle_textfield)
-
-    def get_alarm():
-        erkankung = Erkrankung.get()
-        if unklare_lage_chackbox_state.get():
+    def get_alarm(self):
+        print("Alarmieren")  # Debugging-Ausgabe
+        erkankung = self.Erkrankung.get()
+        if self.unklare_lage_chackbox_state.get():
             erkankung = "Unklare Lage"
             
-        elif sonstiges_state.get():
-            erkankung = textfield.get()
+        elif self.sonstiges_state.get():
+            erkankung = self.textfield.get()
         
-        alarm(erkankung)
-
-        if error == "keine Krankheit ausgewählt":
-            
-            show_error("Bitte wählen Sie eine Krankheit")
+        result = alarm(erkankung)
+    
+        if result == "keine Krankheit ausgewählt":
+                print("Fehler: Keine Krankheit ausgewählt")  # Debugging-Ausgabe
+                self.show_error("Bitte wählen Sie eine Krankheit")
         else:
-            error_label.grid_remove()
-        
+            self.error_label.grid_remove()
 
-            
     
 
+    
 
-    Alarm = tk.Button(frame, text="Alarmieren", command = get_alarm, font=("Arial", 20), bg="red", fg="white")#
-    Alarm.grid(row=5, column=0, padx=10, pady=10)
-        
-def show_page2():
-    button_text.set("Zurück")
-    for widget in frame.winfo_children(): 
-        widget.destroy()
-    Titele = ttk.Label(frame, text="Menu", font=("Arial", 20))
-    Titele.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+    def show_error(self, message):
+        self.error_label.config(text="Error: " + message)
+        print("Error: "+ message)
+        self.error_label.grid(row=0, column=0, padx=10, pady=10)
 
-button1 = ttk.Button(root, textvariable= button_text, command=togle_page)
-button1.grid(row= 0, column=0, padx= 10, pady= 10)
-
-show_page1()
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AlarmApp(root)
+    root.title("Notifer Base")
+    root.geometry("800x600")
+    
+    button_text = tk.StringVar()
+    root.mainloop()
