94d1b9ac27fe8ce86de48072fe3b5f35fc08ac2a
Clemens-Helling <167019903+Clemens-Helling@users.noreply.github.com>
2025-01-17 17:48:42 +0100

    RFID lib hinzugefügt

---
+++ rfid/rfid.py
@@ -0,0 +1,34 @@
+from mfrc522 import SimpleMFRC522
+
+import RPi.GPIO as GPIO
+
+class RFIDReader:
+    def __init__(self):
+        self.reader = SimpleMFRC522()
+
+    def read_data(self):
+        try:
+            print("Place your RFID card near the reader...")
+            id, text = self.reader.read()
+            print(f"ID: {id}")
+            print(f"Text: {text}")
+            return id, text
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            GPIO.cleanup()
+
+    def write_data(self, text):
+        try:
+            print("Place your RFID card near the reader...")
+            self.reader.write(text)
+            print(f"Data written successfully: {text}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            GPIO.cleanup()
+            
+
+if __name__ == "__main__":
+    rfid_reader = RFIDReader()
+    rfid_reader.read_data()
